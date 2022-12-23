ce94b79e03ec9d8f0b6c8d2ca14191dc3e8193d9
yhw2003 <321612813@qq.com>
2022-12-23 13:30:12 +0800

    Changes

---
+++ exercise10/09.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define N 5
+
+//MxN矩阵输入
+int input(int a[][N],int m,int n)
+{
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    return 0;
+}
+
+//输出
+int output(int a[][N],int m,int n)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      printf("%d ", a[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
+
+//求和
+
+int sum(int a[][N],int m,int n)
+{
+    int sum=0;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            sum+=a[i][j];
+        }
+    }
+    return sum;
+}
