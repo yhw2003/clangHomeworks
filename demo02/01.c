7b6b7e2c925b062b39ee7952ddf75bd758c68c14
yhw2003 <321612813@qq.com>
2022-12-27 12:13:33 +0800

    捕获并处理了输入错误，完成了胜负判定

--- demo02/01.c
+++ demo02/01.c
@@ -3,28 +3,66 @@
 
 char const map[] = "\n%d|%d|%d\n-+-+-\n%d|%d|%d\n-+-+-\n%d|%d|%d\n";
 char status[3][3] = {0};
-int point[2] = {0};
 
 extern int start(int bot);
 
 extern int bot(int mode);
 
+extern int calc();
+
 int main()
 {
   while (1)
   {
+    for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 3; j++)
+        status[i][j] = 0;
     int mode = -1;
     system("clear");
     //模式选择
     printf("选择先手：\n1.玩家\n2.电脑\n>");
-    scanf("%d", &mode);
+    
+    while (scanf("%d", &mode) != 1 || mode < 1 || mode > 2)
+    {
+      printf("输入错误，请重新输入：\n>");
+      while (getchar() != '\n');
+    }
     start(mode);
   }
 }
 
+int calc()
+{
+  if (status[0][0] == status[0][1] && status[0][1] == status[0][2] && status[0][0] != 0)
+    return status[0][0];
+  
+  if (status[1][0] == status[1][1] && status[1][1] == status[1][2] && status[1][0] != 0)
+    return status[1][0];
+  
+  if (status[2][0] == status[2][1] && status[2][1] == status[2][2] && status[2][0] != 0)
+    return status[2][0];
+  
+  if (status[0][0] == status[1][0] && status[1][0] == status[2][0] && status[0][0] != 0)
+    return status[0][0];
+  
+  if (status[0][1] == status[1][1] && status[1][1] == status[2][1] && status[0][1] != 0)
+    return status[0][1];
+  
+  if (status[0][2] == status[1][2] && status[1][2] == status[2][2] && status[0][2] != 0)
+    return status[0][2];
+  
+  if (status[0][0] == status[1][1] && status[1][1] == status[2][2] && status[0][0] != 0)
+    return status[0][0];
+  
+  if (status[0][2] == status[1][1] && status[1][1] == status[2][0] && status[0][2] != 0)
+    return status[0][2];
+  
+  return 0;
+}
 
 int start(int mode)
 {
+  int result;
   int turn = 1;
   while (1)
   {
@@ -34,28 +72,73 @@ int start(int mode)
     {
       bot(turn);
       turn = -turn;
+  
+      //判断输赢
+      result = calc();
+  
+      system("clear");
+      printf(map, status[0][0], status[0][1], status[0][2], status[1][0], status[1][1], status[1][2], status[2][0],
+             status[2][1], status[2][2]);
+      if (result)
+      {
+    
+        printf("%d赢了！\n（输入任意字符继续）\n>", result);
+        int c;
+        scanf("%d", &c);
+        break;
+      }
     }
     //打印棋盘
+    system("clear");
     printf(map, status[0][0], status[0][1], status[0][2], status[1][0], status[1][1], status[1][2], status[2][0],
            status[2][1], status[2][2]);
     //接受输入
     int x, y;
     printf("输入坐标：\n(你是%d)>", turn);
-    scanf("%d,%d", &x, &y);
-    if (x <= 0 || x > 3 || y <= 0 || y > 3)
+    while ((scanf("%d,%d", &x, &y) != 2) || x - 1 < 0 || x - 1 > 2 || y - 1 < 0 || y - 1 > 2 ||
+           (status[x - 1][y - 1] != 0))
     {
-      printf("输入错误！\n");
-      continue;
+      printf("输入错误！\n请重新输入>");
+      while (getchar() != '\n');
     }
     x--;
     y--;
     status[x][y] = turn;
     turn = -turn;
+    //打印棋盘
+    system("clear");
+    printf(map, status[0][0], status[0][1], status[0][2], status[1][0], status[1][1], status[1][2], status[2][0],
+           status[2][1], status[2][2]);
+  
+    //判断输赢
+    result = calc();
+    if (result)
+    {
+      printf("%d赢了！\n（输入任意字符继续）\n>", result);
+      int c;
+      scanf("%d", &c);
+      break;
+    }
+  
     if (mode == 1)
     {
       bot(turn);
       turn = -turn;
+      //判断输赢
+      result = calc();
+    
+      system("clear");
+      printf(map, status[0][0], status[0][1], status[0][2], status[1][0], status[1][1], status[1][2], status[2][0],
+             status[2][1], status[2][2]);
+      if (result)
+      {
+        printf("%d赢了！\n（输入任意字符继续）\n>", result);
+        int c;
+        scanf("%d", &c);
+        break;
+      }
     }
+  
   }
 }
 
